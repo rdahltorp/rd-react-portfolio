4c305c49e4ec61db112f001ff96f6bc679041f3a
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2023-01-15 10:34:31 -0700

    Started work on portfolio page

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -3,7 +3,29 @@ import React from "react";
 export default function Portfolio() {
     return (
         <div>
-            <h1>This is where all my projects will go</h1>
+            <h1>Previous Work</h1>
+            <div className="Project-1 box">
+
+            </div>
+            <div className="Project-2 box">
+
+            </div>
+            <div className="Project-3 box">
+
+            </div>
+            <div className="Project-4 box">
+
+            </div>
+            <div className="Project-5 box">
+                <p>VeggieBook</p>
+                <p href='https://github.com/bornoflightning/veggiebook' target="_blank" rel="noreferrer">Repo</p>
+                <p href='https://hidden-lake-21615.herokuapp.com/' target="_blank" rel="noreferrer">Live App</p>
+            </div>
+            <div className="Project-6 box">
+                <p>Park Search</p>
+                <p href='https://github.com/dcavinder/park_search' target="_blank" rel="noreferrer">Repo</p>
+                <p href='https://hidden-lake-21615.herokuapp.com/' target="_blank" rel="noreferrer">Live App</p>
+            </div>
         </div>
     )
 }
