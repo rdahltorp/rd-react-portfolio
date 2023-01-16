bf71e3c3fbdeaa742e75f681848b2e197a7f494c
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2023-01-15 18:43:44 -0700

    Added design of header and footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,13 +1,45 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
-export default function Footer() {
-    return (
-        <div>
-            <ul>
-                <li><a href="https://github.com/rdahltorp" target="_blank" rel="noreferrer">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/reeddahltorp/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-                <li><a href="https://stackoverflow.com/users/21008616/reed-dahltorp" target="_blank" rel="noreferrer">StackOverflow</a></li>
-            </ul>
-        </div>
-    )
+
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ alignItems: 'center', }}>
+      <BottomNavigation
+        showLabels
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{background: '#4A9666'}}
+      >
+        <BottomNavigationAction sx={{color: 'white'}} label="GitHub" icon={<GitHubIcon />} href="https://github.com/rdahltorp" target="_blank" rel="noreferrer" />
+        <BottomNavigationAction sx={{color: 'white'}} label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/in/reeddahltorp/" target="_blank" rel="noreferrer" />
+        <BottomNavigationAction sx={{color: 'white'}} label="Twitter" icon={<TwitterIcon />} href="https://twitter.com/rdahltorp" target="_blank" rel="noreferrer" />
+      </BottomNavigation>
+    </Box>
+  );
 }
+
+
+
+// import React from "react";
+
+// export default function Footer() {
+//     return (
+//         <div>
+//             <ul>
+//                 <li><a href="https://github.com/rdahltorp" target="_blank" rel="noreferrer">GitHub</a></li>
+//                 <li><a href="https://www.linkedin.com/in/reeddahltorp/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+//                 <li><a href="https://stackoverflow.com/users/21008616/reed-dahltorp" target="_blank" rel="noreferrer">StackOverflow</a></li>
+//             </ul>
+//         </div>
+//     )
+// }
--- src/components/Header/navigation.js
+++ src/components/Header/navigation.js
@@ -1,49 +1,203 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
-export default function Nav({ currentPage, handlePageChange }) {
-    return (
-        <header>
-            <h1>Reed Dahltorp</h1>
-            <nav>
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <a 
-                            href="#AboutMe"
-                            onClick={() => handlePageChange('AboutMe')}
-                            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link' }
-                        >
-                            About Me
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a 
-                            href="#Portfolio"
-                            onClick={() => handlePageChange('Portfolio')}
-                            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link' }
-                        >
-                            Portfolio
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a 
-                            href="#Resume"
-                            onClick={() => handlePageChange('Resume')}
-                            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link' }
-                        >
-                            Resume
-                        </a>
-                    </li>
-                    <li className="Contact">
-                        <a 
-                            href="#Contact"
-                            onClick={() => handlePageChange('Contact')}
-                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link' }
-                        >
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+
+
+function ResponsiveAppBar({ currentPage, handlePageChange }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{
+        background: '#4A9666'
+      }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          {/* Handles desktop headline */}
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Reed Dahltorp
+          </Typography>
+            
+            {/* Handles the mobile menu burger icon and menu options */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            {/* Icon */}
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            {/* Menu options in mobile */}
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+                <MenuItem onClick={() => handlePageChange('AboutMe') && handleCloseNavMenu}>
+                  <Typography textAlign="center">About Me</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Portfolio') && handleCloseNavMenu}>
+                  <Typography textAlign="center">Portfolio</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Resume') && handleCloseNavMenu}>
+                  <Typography textAlign="center">Resume</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Contact') && handleCloseNavMenu}>
+                  <Typography textAlign="center">Contact</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+          
+          {/* Handles mobile headline */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Reed Dahltorp
+          </Typography>
+          
+          {/* Handles the menu options in desktop */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <MenuItem onClick={() => handlePageChange('AboutMe')}>
+                  <Typography textAlign="center">About Me</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Portfolio')}>
+                  <Typography textAlign="center">Portfolio</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Resume')}>
+                  <Typography textAlign="center">Resume</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handlePageChange('Contact')}>
+                  <Typography textAlign="center">Contact</Typography>
+                </MenuItem>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
+export default ResponsiveAppBar;
+
+//OLD MENU
+//import React from "react";
+
+
+// export default function Nav({ currentPage, handlePageChange }) {
+//     return (
+//         <header>
+//             <h1>Reed Dahltorp</h1>
+//             <nav>
+                // <ul className="nav nav-tabs">
+                //     <li className="nav-item">
+                //         <a 
+                //             href="#AboutMe"
+                //             onClick={() => handlePageChange('AboutMe')}
+                //             className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link' }
+                //         >
+                //             About Me
+                //         </a>
+                //     </li>
+                //     <li className="nav-item">
+                //         <a 
+                //             href="#Portfolio"
+                //             onClick={() => handlePageChange('Portfolio')}
+                //             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link' }
+                //         >
+                //             Portfolio
+                //         </a>
+                //     </li>
+                //     <li className="nav-item">
+                //         <a 
+                //             href="#Resume"
+                //             onClick={() => handlePageChange('Resume')}
+                //             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link' }
+                //         >
+                //             Resume
+                //         </a>
+                //     </li>
+                //     <li className="Contact">
+                //         <a 
+                //             href="#Contact"
+                //             onClick={() => handlePageChange('Contact')}
+                //             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link' }
+                //         >
+                //             Contact
+                //         </a>
+                //     </li>
+                // </ul>
+//             </nav>
+//         </header>
+//     )
+// }
